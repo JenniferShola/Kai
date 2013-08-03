05f7495a49173a81a70bcd92641eb7f6db6caf28
Shola Oyedele <sjoyedele@gmail.com>
2013-08-03 05:08:07 -0700

    new user table view custom cell

---
+++ Kai/KYUserTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  KYUserTableViewCell.h
+//  Kai
+//
+//  Created by Shola Oyedele on 8/3/13.
+//  Copyright (c) 2013 Shola Oyedele. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KYUserTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *userPhoto;
+@property (weak, nonatomic) IBOutlet UILabel *profileName;
+
+@end
